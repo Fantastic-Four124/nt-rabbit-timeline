17f5b94ae9cce1fe3dd84a08556f48cdad793204
Zhengyang Zhou <zhengyjo@brandeis.edu>
2018-04-22 22:31:34 -0400

    follow service

--- timeline_server.rb
+++ timeline_server.rb
@@ -108,7 +108,7 @@ class TimelineServer
   def generate_potential_tweet_list(user_id,leader_id)
     potential_tweet_list = []
     current_timeline = []
-    if $tweet_redis.get(user_id.to_s + '_timeline')
+    if $tweet_redis.llen(user_id.to_s + '_timeline') > 0
       previous_timeline = []
       $tweet_redis.lrange(user_id.to_s + "_timeline", 0, -1).each do |tweet|
         previous_timeline << JSON.parse(tweet)
